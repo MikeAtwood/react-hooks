386e1c19c350e8985f148402062509a945b13ec9
Deadpool1 <atwood169@yahoo.com>
2023-10-05 19:36:46 -0500

    updated useEffect

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Clock = () => {
     const [counter, setCounter] =useState(0)
 
-    setInterval(() => {
-        setCounter(count => count + 1)
-    }, 1000)
+    useEffect(() =>{
+        const key = setInterval(() => {
+            setCounter(count => count + 1)
+        }, 1000)
+
+        return () => {
+            clearInterval(key)
+        }
+    }, [])
+    
 
     return (
         <p>{counter} seconds have passed</p>
