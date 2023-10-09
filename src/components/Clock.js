991080e48124a63b735fc5b198169bacec4c70c0
Deadpool1 <atwood169@yahoo.com>
2023-10-09 18:21:46 -0500

    added comments

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,10 +4,12 @@ const Clock = () => {
     const [counter, setCounter] =useState(0)
 
     useEffect(() =>{
+        // This executes the side effect (starts the clock)
         const key = setInterval(() => {
             setCounter(count => count + 1)
         }, 1000)
-
+        
+        // cleanup function
         return () => {
             clearInterval(key)
         }
